82196a1012153261f1b3879f447a7e1619a51c2d
la9527 <la9527@sk.com>
2020-07-09 17:14:40 +0900

    [DEV] add post install script

---
+++ scripts/post-install.js
@@ -0,0 +1,54 @@
+const os = require("os");
+const fs = require("fs");
+
+function fileAppendText( insertText, fileName, matchText ) {
+    try {
+        if ( fs.existsSync(fileName) ) {
+            let data = fs.readFileSync( fileName, "utf8" );
+            let lines = data.split("\n");
+            let find = false;
+            lines = lines.map( (item) => {
+                if ( item.match( matchText ) ) {
+                    find = true;
+                    return insertText;
+                }
+                return item;
+            });
+            if ( !find ) {
+                lines.push( insertText );
+                lines.push( "" );
+            }
+            fs.writeFileSync( fileName, lines.join("\n") );
+            return true;
+        }
+    } catch( e ) {
+        console.error( e );
+    }
+    return false;
+}
+
+
+function scriptUpdate() {
+    let basePath = fs.realpathSync( __dirname );
+    basePath = fs.realpathSync( basePath + "/..");
+
+    try {
+        console.log( `Update: ${basePath}/bin/mdir -> ${basePath}/bin/mdir.js` );
+        fs.copyFileSync( basePath + "/bin/mdir", basePath + "/bin/mdir.js" );
+        
+        console.log( `Update: ${basePath}/bin/mdir.sh -> ${basePath}/bin/mdir` );
+        fs.copyFileSync( basePath + "/bin/mdir.sh", basePath + "/bin/mdir" );
+
+        [ os.homedir() + "/.bashrc", os.homedir() + "/.zshrc" ].forEach( item => {
+            if ( fileAppendText( `alias mdir=". ${basePath}/bin/mdir"`, item, /^alias mdir/ ) ) {
+                console.log( `Update: alias mdir -> ${item}` );
+            }
+        });
+    } catch( e ) {
+        console.error( e );
+    }
+}
+
+if ( os.platform() !== "win32" ) {
+    scriptUpdate();
+}
