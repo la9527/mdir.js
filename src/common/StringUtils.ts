f9a2256339aede4283f9929109c75b5458d09f31
la9527 <la9527@sk.com>
2020-07-16 14:08:24 +0900

    [DEV] editor update.

--- src/common/StringUtils.ts
+++ src/common/StringUtils.ts
@@ -1,4 +1,6 @@
 import { sprintf } from "sprintf-js";
+import { strWidth } from "neo-blessed/lib/unicode";
+import { timeStamp } from "console";
 
 export class StringUtils {
     static toregular(num: any ) {
@@ -12,9 +14,9 @@ export class StringUtils {
             ret = ret.replace(reg, "$1,$2");
         }
         return ret;
-     }
+    }
 
-     static sizeConvert( size: number, color: boolean = true, digit = 2 ) {
+    static sizeConvert( size: number, color: boolean = true, digit = 2 ) {
         let textSize = null;
         if ( size >= 1000000000000 ) {
             textSize = sprintf("%9." + digit + "f" + (color ? "{yellow-fg}T{/yellow-fg}" : "T"), size / 1099511627776);
@@ -26,16 +28,119 @@ export class StringUtils {
             textSize = sprintf("%10s", StringUtils.toregular(size));
         }
         return textSize;
-     }
+    }
 
-     static ellipsis( text: string, maxWidth: number ) {
-         if ( !text ) {
+    static ellipsis( text: string, maxWidth: number ) {
+        if ( !text ) {
             return null;
-         }
-         if ( text.length <= maxWidth ) {
+        }
+        if ( text.length <= maxWidth ) {
             return text;
-         }
-         let cutPos = Math.floor(maxWidth / 2);
-         return text.substr(0, cutPos - 3) + "..." + text.substr( text.length - cutPos );
-     }
+        }
+        let cutPos = Math.floor(maxWidth / 2);
+        return text.substr(0, cutPos - 3) + "..." + text.substr( text.length - cutPos );
+    }
+
+    static scrSubstr( text: string, firstPos: number, len: number = -1 ) {
+        let pos = firstPos;
+        let strlen = 0;
+        let resText = "";
+        do {
+            pos++;
+            resText += text[pos];
+            strlen += strWidth( text[pos] );
+            if ( len > -1 && strlen >= len ) break;
+        } while( pos < text.length );
+        return resText;
+    }
+
+    // ("123456", 3, 1) ==> "12456"
+    // ("123456", 3, "7") ==> "123756"
+    // ("123456", 3, "7", true) ==> "1237456"
+    static scrStrReplace( text: string, firstPos: number, removeLenChStr: string | number = -1, isInsert: boolean = false ): string {
+        let resultText = StringUtils.scrSubstr(text, 0, firstPos);
+        if ( typeof(removeLenChStr) == "string" ) {
+            if ( isInsert ) {
+                return resultText + removeLenChStr + StringUtils.scrSubstr(text, firstPos);
+            }
+            return resultText + removeLenChStr + StringUtils.scrSubstr(text, firstPos + strWidth(removeLenChStr));
+        }
+        return resultText + StringUtils.scrSubstr(text, firstPos + removeLenChStr);
+    }
+}
+
+export class StringLineToken {
+    private tokens: string[] = [];
+    private _source: string = null;
+
+    private _width: number;
+    private _curLine: number;
+
+    get curLine() {
+        return this._curLine;
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get source() {
+        return this._source;
+    }
+
+    setString( source: string, viewWidth = 80 ) {
+        let nextLine = false;
+        let line = null;
+        let viewStr = null;
+        let lineStr = source;
+        
+        this._source = source;
+        this.tokens = [];
+        this._width = viewWidth;
+        this._curLine = 0;
+
+        if ( !source ) {
+            return;
+        }
+
+        do {
+            let strlen = strWidth(lineStr);
+            if ( strlen <= viewWidth ) {
+                viewStr = lineStr;
+                nextLine = true;
+            } else {
+                viewStr = StringUtils.scrSubstr( lineStr, 0, viewWidth );
+                lineStr = StringUtils.scrSubstr( lineStr, viewWidth, strlen - viewWidth );
+                nextLine = false;
+            }
+
+            this.tokens.push( viewStr );
+        } while( !nextLine );
+    }
+
+    setLineData( text, line ) {
+        if ( this.tokens.length <= line ) {
+            this.tokens.push( text );
+        } else {
+            this.tokens[line] = text;
+        }
+    }
+
+    get() {
+        return this.tokens[this._curLine];
+    }
+
+    size() {
+        return this.tokens.length;
+    }
+
+    next(isCheckOnly: boolean = false): boolean {
+        if ( this.tokens.length > this._curLine ) {
+            if ( !isCheckOnly ) {
+                this._curLine++;
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- src/editor/Editor.ts
+++ src/editor/Editor.ts
@@ -1,17 +1,19 @@
-import { IDecPrivateModes } from "panel_blassed/xterm/common/Types";
+import { strWidth } from "neo-blessed/lib/unicode";
 import { DoData } from "./EditorClipboard";
 import { File } from "../common/File";
 import fs from "fs";
 import { Logger } from "../common/Logger";
+import { StringUtils } from "common/StringUtils";
+import { StringLineToken } from '../common/StringUtils';
 
 const log = Logger( "editor" );
 
 interface IEditorBuffer {
-    textLine: number;       // Text Position
-    viewLine: number;       // screen view position
-    nextLineNum: number;    // if over the one line, line number.
-    isNext: boolean;        // Is this line over the one line?
-    text: string;
+    textLine ?: number;       // Text Position
+    viewLine ?: number;       // screen view position
+    nextLineNum ?: number;    // if over the one line, line number.
+    isNext ?: boolean;        // Is this line over the one line?
+    text ?: string;
 };
 
 interface IEditSelect {
@@ -27,6 +29,8 @@ enum EDIT_MODE {
     BLOCK,            /// Block Select Mode
     SHIFT_SELECT    /// Shift Mode
 };
+
+const TABCONVCHAR = " ";
 
 export abstract class Editor {
     line: number = 0;
@@ -77,9 +81,12 @@ export abstract class Editor {
     }
 
     abstract postLoad(): void;
-    abstract postUpdateLines( line: number, height: number ): void;
+    abstract postUpdateLines( line?: number, height?: number ): void;
 
-    public selectSort( editSelect: IEditSelect ) {
+    abstract inputBox(title, text): Promise<string>;
+    abstract messageBox(title, text, buttons ?: [ string ]): Promise<string>;
+
+    public selectSort(editSelect: IEditSelect) {
 
     }
 
@@ -88,7 +95,71 @@ export abstract class Editor {
     }
 
     public screenMemSave( line: number, column: number ) {
+        if ( this.curLine > 0 ) {
+            if ( this.curLine >= this.buffers.length) this.curLine = this.buffers.length - 1;
+            if ( this.curLine <= this.firstLine ) this.firstLine = this.firstLine - 1;
+            if ( this.firstLine <= 0 ) this.firstLine = 0;
+            if ( this.lastLine - this.firstLine >= 10 && this.lastLine - this.curLine <= 0 ) {
+                if ( this.viewBuffers.length >= this.line ) {
+                    if ( this.firstLine <= this.buffers.length ) {
+                        this.firstLine++;
+                    }
+                    if ( this.buffers.length <= this.line - 5 ) {
+                        this.firstLine = 0;
+                    }
+                }
+            }
+        }
 
+        log.debug("firstLine [%d] [%d]", this.curLine, this.firstLine);
+
+        let strLineToken = new StringLineToken();
+
+        for(;;) {
+            let viewLine = this.firstLine;
+            if (viewLine < 0) return;
+    
+            this.viewBuffers = [];
+
+            let isNext = false;
+            
+            for ( let t = 0; t < line; t++ ) {
+                if ( !strLineToken.next(true) ) {
+                    if ( viewLine >= this.buffers.length ) break;
+                    strLineToken.setString( this.buffers[viewLine++], this.column );
+                }
+
+                isNext = strLineToken.size() - 1 !== strLineToken.curLine;
+
+                let viewStr = strLineToken.get();
+
+                let lineInfo: IEditorBuffer = {};
+                lineInfo.viewLine = t;
+                lineInfo.textLine = viewLine - 1;
+                lineInfo.text = viewStr;
+                lineInfo.isNext = isNext;
+                lineInfo.nextLineNum = strLineToken.curLine;
+
+                // let nLineBeginPos = lineInfo.nextLineNum * column;
+                // let nLineEndPos = (lineInfo.nextLineNum * column) + column;                
+                this.viewBuffers.push( lineInfo );
+                strLineToken.next();
+            }
+
+            this.lastLine = viewLine - 1;
+
+            if ( this.viewBuffers.length > line - 3 ) {
+                if ( this.lastLine === this.curLine && this.viewBuffers[this.lastLine].isNext ) {
+                    this.firstLine++;
+                    continue;
+                }
+                if ( this.lastLine < this.curLine ) {
+                    this.firstLine++;
+                    continue;
+                }
+            }
+            break;
+        }
     }
     
     setViewTitle( title = "" ) {
@@ -187,10 +258,74 @@ export abstract class Editor {
         this.isLineNumView = !this.isLineNumView;
     }
 
-    keyLeft() {}
-    keyRight() {}
-    keyUp() {}
-    keyDown() {}
+    keyLeft() {
+        if ( this.curColumn > 0 ) {
+            let cul = 0;
+            for ( let i = 0; i < this.tabSize; i++ ) {
+                this.curColumn--;
+                cul++;
+                if ( this.curColumn <= 0 && TABCONVCHAR === StringUtils.scrSubstr(this.buffers[this.curLine], this.curColumn, 1 ) ) {
+                    break;
+                }
+            }
+            if ( cul != this.tabSize && cul > 1 ) {
+                this.curColumn++;
+            }
+        } else if ( this.curLine > 0) {
+            this.curColumn = strWidth(this.buffers[ --this.curLine ]);
+        }
+        this.keyPressCommon();
+    }
+
+    keyRight() {
+        let str = this.buffers[this.curLine];
+        let strlen = strWidth(str);
+        if ( strlen > this.curColumn ) {
+            for ( let i = 0; i < this.tabSize; i++ ) {
+                this.curColumn++;
+                if ( this.curColumn <= strlen ||
+                    TABCONVCHAR === StringUtils.scrSubstr(str, this.curColumn, 1 ) ) {
+                    break;
+                }
+                if ( i === 0 ) {
+                    if ( TABCONVCHAR !== StringUtils.scrSubstr(str, this.curColumn - 1, 1 ) ) {
+                        break;
+                    }
+                }
+            }
+        } else if ( strlen === this.curColumn && this.curLine !== this.buffers.length - 1 ) {
+            this.curLine++;
+            this.curColumn = 0;
+        }
+        this.keyPressCommon();
+    }
+
+    keyUp() {
+        if ( this.curLine > 0 ) this.curLine--;
+
+        if ( this.curColumnMax < this.curColumn ) {
+            this.curColumnMax = this.curColumn;
+        } else {
+            this.curColumn = this.curColumnMax;
+        }
+
+        let strlen = strWidth(this.buffers[this.curLine]);
+        if ( strlen < this.curColumn ) {
+            this.curColumn = strlen;
+        } else {
+            this.keyRight();
+            this.keyLeft();
+        }
+
+        this.editSelect.x2 = this.curColumn;
+        this.editSelect.y2 = this.curLine;
+        if ( this.editMode === EDIT_MODE.SHIFT_SELECT ) this.editMode = EDIT_MODE.EDIT;
+    }
+
+    keyDown() {
+
+    }
+
     keyShiftLeft() {}
     keyShiftRight() {} 
     keyShiftUp() {}
@@ -201,19 +336,143 @@ export abstract class Editor {
     }
 
     keyDelete() {
-        
+        if ( this.isReadOnly ) return;
+
+        if ( this.editMode !== EDIT_MODE.EDIT ) {
+            this.selectDel();
+            this.editMode = EDIT_MODE.EDIT;
+        }
+
+        let line = this.buffers[this.curLine];
+        if ( this.curColumn < strWidth(line) ) {
+            this.doInfo.push( new DoData(this.curLine, this.curColumn, [ line ]));
+
+            let temp = StringUtils.scrSubstr(line, this.curColumn, this.tabSize);
+            line = StringUtils.scrStrReplace(this.buffers[this.curLine], this.curColumn, temp === TABCONVCHAR.repeat(temp.length) ? temp.length : 1);
+            
+            this.buffers[this.curLine] = line;
+            this.postUpdateLines(this.curLine);
+        } else if ( this.curLine + 1 < this.buffers.length ) {
+            let line2 = this.buffers[this.curLine + 1];
+            this.doInfo.push( new DoData(this.curLine, this.curColumn, [ line2 ] ));
+
+            this.buffers[this.curLine] = line + line2;
+            this.buffers.splice( this.curLine, 1 );
+            this.postUpdateLines(this.curLine);
+        }
+        this.curColumnMax = this.curColumn;
+
+        if ( this.buffers.length === 0 ) {
+            this.buffers.push( "" );
+        }
     }
 
     keyBS() {
-        
+        if ( this.isReadOnly ) return;
+
+        if ( this.editMode !== EDIT_MODE.EDIT ) {
+            this.selectDel();
+            this.editMode = EDIT_MODE.EDIT;
+
+            this.curLine = this.editSelect.y1;
+            this.curColumn = this.editSelect.x1;
+            this.curColumnMax = this.curColumn;
+        }
+
+        if ( this.curLine === 0 && this.curColumn === 0 ) return;
+
+        if ( this.buffers.length > this.curLine ) {
+            let line = this.buffers[this.curLine];
+
+            let line2;
+            if ( this.curColumn === 0 && this.buffers.length > 0 && this.curLine > 0 ) {
+                line2 = this.buffers[this.curLine - 1];
+                this.doInfo.push( new DoData(this.curLine - 1, this.curColumn, [ line2, line ] ));
+
+                let tmpLine2Width = strWidth( line2 );
+                this.buffers[ this.curLine - 1 ] = line2 + line;
+                this.buffers.splice( this.curLine, 1 );
+
+                this.postUpdateLines(this.curLine);
+                this.keyUp();
+                this.curColumn = tmpLine2Width;
+            } else {
+                let strSize = strWidth( this.buffers[ this.curLine ] );
+                if ( this.curColumn <= strSize ) {
+                    this.doInfo.push( new DoData(this.curLine, this.curColumn, [ line ] ));
+
+                    let tabCheck = "";
+                    if ( this.curColumn - this.tabSize >= 0 ) {
+                        tabCheck = StringUtils.scrSubstr(this.buffers[this.curLine], this.curColumn - this.tabSize, this.tabSize);
+                    } else {
+                        tabCheck = StringUtils.scrSubstr(this.buffers[this.curLine], 0, this.curColumn);
+                    }
+                    if ( tabCheck === TABCONVCHAR.repeat(tabCheck.length) ) {
+                        line2 = StringUtils.scrStrReplace(this.buffers[this.curLine], this.curColumn - tabCheck.length, tabCheck.length);
+                        this.curColumn -= tabCheck.length;
+                    } else {
+                        line2 = StringUtils.scrStrReplace(this.buffers[this.curLine], this.curColumn - 1, 1);
+                        this.curColumn -= 1;
+                    }
+                }
+            }
+            this.buffers[ this.curLine ] = line2;
+            this.postUpdateLines( this.curLine );
+
+            this.editSelect.x2 = this.curColumn;
+            this.editSelect.y2 = this.curLine;
+        }
+        this.curColumnMax = this.curColumn;
     }
 
     keyTab() {
+        if ( this.isReadOnly ) return;
 
+        if ( this.editMode !== EDIT_MODE.EDIT ) {
+            const tabStr = TABCONVCHAR.repeat(this.tabSize);
+
+            this.selectSort( this.editSelect );
+
+            let save: string[] = [];
+            for ( let y = this.editSelect.y1; y <= this.editSelect.y2; y++ ) {
+                save.push( this.buffers[ y ] );
+            }
+            this.doInfo.push( new DoData(this.editSelect.y1, 0, save, -1 ));
+            
+            for ( let y = this.editSelect.y1; y <= this.editSelect.y2; y++ ) {
+                this.buffers[y] = tabStr + this.buffers[y];
+            }
+
+            this.postUpdateLines( this.editSelect.y1, this.editSelect.y2 - this.editSelect.y1 + 1);
+            this.screenMemSave( this.line, this.column );
+        }
+
+        let tabSize = 4 - (this.curColumn % 4);
+        this.inputData( TABCONVCHAR.repeat(tabSize) );
+        this.screenMemSave( this.line, this.column );
     }
 
     keyUntab() {
+        if ( this.isReadOnly ) return;
 
+        if ( this.editMode !== EDIT_MODE.EDIT ) {
+            const tabStr = TABCONVCHAR.repeat(this.tabSize);
+
+            this.selectSort( this.editSelect );
+
+            let save: string[] = [];
+            for ( let y = this.editSelect.y1; y <= this.editSelect.y2; y++ ) {
+                save.push( this.buffers[ y ] );
+            }
+            this.doInfo.push( new DoData(this.editSelect.y1, 0, save, -1 ));
+
+            for ( let y = this.editSelect.y1; y <= this.editSelect.y2; y++ ) {
+                if ( tabStr === this.buffers[y].substr(0, tabStr.length) ) {
+                    this.buffers[y] = this.buffers[y].substr(tabStr.length);
+                }
+            }
+        }
+        this.screenMemSave( this.line, this.column );
     }
 
     indentMode() {
@@ -221,18 +480,117 @@ export abstract class Editor {
     }
 
     inputData( textStr: string ) {
+        if ( this.isReadOnly ) return;
 
+        if ( this.editMode !== EDIT_MODE.EDIT ) {
+            this.selectDel();
+            this.editMode = EDIT_MODE.EDIT;
+        }
+
+        if ( this.curLine < this.buffers.length ) {
+            let line = this.buffers[this.curLine];
+            this.doInfo.push( new DoData(this.curLine, this.curColumn, [line]) );
+
+            if ( this.isInsert ) {
+                line = StringUtils.scrSubstr( line, 0, this.curColumn) + textStr + StringUtils.scrSubstr( line, this.curColumn );
+            } else {
+                line = StringUtils.scrSubstr( line, 0, this.curColumn) + textStr + StringUtils.scrSubstr( line, this.curColumn + strWidth(textStr) );
+            }
+            this.buffers[this.curLine] = line;
+            this.curColumn += strWidth(textStr);
+        }
+        this.curColumnMax = this.curColumn;
     }
         
-    keyHome() {}
-    keyEnd() {}
+    keyHome() {
+        let line = this.buffers[this.curLine];
+        let ne = 0, old = this.curColumn;
+        for ( let n = 0; n < line.length; n++ ) {
+            if (line[n] !== ' ' && line[n] !== TABCONVCHAR) {
+                ne = n;
+                break;
+            }
+        }
+        this.curColumn = old === ne ? 0 : ne;
+        this.keyPressCommon();
+    }
+
+    keyEnd() {
+        this.curColumn = strWidth( this.buffers[this.curLine] );
+        this.keyPressCommon();
+    }
+
     keyPgUp() {}
     keyPgDn() {}
-    keyEnter() {}
+    
+    keyEnter() {
+        if ( this.isReadOnly ) return;
+
+        if ( this.editMode !== EDIT_MODE.EDIT ) {
+            this.selectDel();
+            this.editMode = EDIT_MODE.EDIT;
+        }
+
+        let line = this.buffers[this.curLine];
+        let p1 = "";
+        if ( this.indentMode ) {
+            for ( let n = 0; n < line.length; n++ ) {
+                if (line[n] !== ' ' && line[n] !== TABCONVCHAR) {
+                    p1 = line.substr(0, n);
+                    break;
+                }
+            }
+        }
+
+        this.doInfo.push( new DoData(this.curLine, this.curColumn, [line], 2) );
+
+        if ( this.buffers.length > this.curLine ) {
+            this.buffers[this.curLine] = StringUtils.scrSubstr(line, 0, this.curColumn);
+            let line3 = p1 + StringUtils.scrSubstr(line, this.curColumn);
+            this.buffers.splice( this.curLine, 0, line3 );
+            this.postUpdateLines(this.curLine);
+        } else {
+            this.buffers.push(p1);
+            this.screenMemSave( this.line, this.column );
+        }
+        this.curColumn = p1.length;
+        this.curColumnMax = this.curColumn;
+        this.keyDown();
+    }
+
     keyMouse() {}
-    gotoLine() {}
-    gotoFirst() {}
-    gotoEnd() {}
+    
+    async gotoLinePromise() {
+        const result = await this.inputBox( "Go to Line Number", "Enter the line number to move." );
+        let number = -1;
+        try {
+            number = parseInt( result );
+        } catch ( e ) {
+            await this.messageBox( "ERROR", "Invalid input number" );
+        }
+        if ( number > -1 && number < this.buffers.length ) {
+            this.curLine = number - 1;
+            if ( this.curLine <= 0 ) this.curLine = 0;
+            this.firstLine = this.curLine - 10;
+            if ( this.firstLine <= 0 ) this.firstLine = 0;
+        } else {
+            this.curLine = this.buffers.length - 1;
+            this.firstLine = this.curLine - 10;
+        }
+        this.editMode = EDIT_MODE.EDIT;
+    }
+
+    gotoFirst() {
+        this.curLine = 0;
+        this.firstLine = 0;
+        this.editMode = EDIT_MODE.EDIT;
+    }
+
+    gotoEnd() {
+        this.curLine = this.buffers.length - 1;
+        this.firstLine = this.curLine - 10;
+        this.editMode = EDIT_MODE.EDIT;
+    }
     
     copy() {
 
@@ -296,5 +654,14 @@ export abstract class Editor {
 
     isEditMode() {
         return this.editMode === EDIT_MODE.EDIT;
+    }
+
+    keyPressCommon() {
+        this.editSelect.x2 = this.curColumn;
+        this.editSelect.y2 = this.curLine;
+        this.curColumnMax = this.curColumn;
+        if ( this.editMode === EDIT_MODE.SHIFT_SELECT ) {
+            this.editMode = EDIT_MODE.EDIT;
+        }
     }
 };
